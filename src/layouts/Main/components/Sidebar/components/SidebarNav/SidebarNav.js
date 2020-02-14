668ce442085570f1ca9cc0f99eeb9d1ad808005d
Benzer Bett <benzerbett@gmail.com>
2020-02-14 10:39:58 +0300

    sidebar navlinks

--- src/layouts/Main/components/Sidebar/components/SidebarNav/SidebarNav.js
+++ src/layouts/Main/components/Sidebar/components/SidebarNav/SidebarNav.js
@@ -6,7 +6,7 @@ import clsx from 'clsx';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/styles';
 import { List, ListItem, ListItemText, Button, colors, Collapse, ListSubheader, Divider, ListItemIcon } from '@material-ui/core';
-import ChevronLeftOutlinedIcon from '@material-ui/icons/ChevronLeftOutlined'; import ExpandMore from '@material-ui/icons/ExpandMore';
+import ChevronLeftOutlinedIcon from '@material-ui/icons/ChevronLeftOutlined'; import ChevronRightOutlined from '@material-ui/icons/ChevronRightOutlined';
 
 const useStyles = makeStyles(theme => ({
   root: {},
@@ -19,6 +19,8 @@ const useStyles = makeStyles(theme => ({
     color: colors.blueGrey[800],
     padding: '10px 8px',
     justifyContent: 'flex-start',
+    textAlign: 'left',
+    display: 'block',
     textTransform: 'none',
     letterSpacing: 0,
     width: '100%',
@@ -38,6 +40,15 @@ const useStyles = makeStyles(theme => ({
     '& $icon': {
       color: theme.palette.primary.main
     }
+  },
+  nav: {
+    textTransform: 'capitalize',
+    fontWeight: 'normal',
+    width: '100%',
+    textAlign: 'left'
+  },
+  chevrons: {
+    color: '#cccccc'
   }
 }));
 
@@ -56,98 +67,88 @@ const SidebarNav = props => {
   const classes = useStyles();
 
   //multi menu
-  const [open1, setOpen1] = React.useState(false);
-  const handleClick1 = () => {
-    setOpen1(!open1);
-  };
-  const [open2, setOpen2] = React.useState(false);
-  const handleClick2 = () => {
-    setOpen2(!open2);
-  };
-  const [open3, setOpen3] = React.useState(false);
-  const handleClick3 = () => {
-    setOpen3(!open3);
-  };
-  const [open4, setOpen4] = React.useState(false);
-  const handleClick4 = () => {
-    setOpen4(!open4);
-  };
-  const [open5, setOpen5] = React.useState(false);
-  const handleClick5 = () => {
-    setOpen5(!open5);
-  };
+  const [open1, setOpen1] = React.useState(false); const handleClick1 = () => setOpen1(!open1);
+  const [open2, setOpen2] = React.useState(false); const handleClick2 = () => setOpen2(!open2);
+  const [open3, setOpen3] = React.useState(false); const handleClick3 = () => setOpen3(!open3);
+  const [open4, setOpen4] = React.useState(false); const handleClick4 = () => setOpen4(!open4);
+  const [open5, setOpen5] = React.useState(false); const handleClick5 = () => setOpen5(!open5);
   //multi menu
 
   return (
-    <List
-      {...rest}
-      className={clsx(classes.root, className)}
-    >
-      {pages.map(page => (
-        <ListItem className={classes.item} disableGutters key={page.title} >
-          <Button activeClassName={classes.active} className={classes.button} component={CustomRouterLink} to={page.href} >
-            {page.title}
-          </Button>
+    <List {...rest} className={clsx(classes.root, className)} >
+        <ListItem className={classes.item} disableGutters >
+          <Button activeClassName={classes.active} className={classes.button} component={CustomRouterLink} to="/dashboard" > Dashbord </Button>
         </ListItem>
-      ))}
       {/* <br/> */}
       <ListSubheader style={{color: '#aaaaaa', fontSize: 'small'}}>COUNTY</ListSubheader>
       <Divider/>
       {/* ==============multi menu=============== */}
         <ListItem button onClick={handleClick1}>
           <ListItemText primary="Stock Status" />
-          {open1 ? <ChevronLeftOutlinedIcon /> : <ExpandMore />}
+          {open1 ? <ChevronLeftOutlinedIcon className={classes.chevrons} /> : <ChevronRightOutlined className={classes.chevrons} />}
         </ListItem>
         <Collapse in={open1} timeout="auto" unmountOnExit>
           <List component="div" disablePadding>
-            <ListItem button> &nbsp;<ListItemText primary="Artesunate Inj." /> </ListItem>
-            <ListItem button> &nbsp;<ListItemText primary="Artemether L." /> </ListItem>
-            <ListItem button> &nbsp;<ListItemText primary="Sulphadoxine P." /> </ListItem>
-            <ListItem button> &nbsp;<ListItemText primary="R.D. Tests" /> </ListItem>
-            <ListItem button> &nbsp;<ListItemText primary="All Commodities" /> </ListItem>
+            <ListItem className={classes.item} disableGutters> <Button className={classes.nav} component={CustomRouterLink} to="/users"> Artesunate Inj.</Button></ListItem>
+            <ListItem className={classes.item} disableGutters> <Button className={classes.nav} component={CustomRouterLink} to="/users"> Artemether L.</Button></ListItem>
+            <ListItem className={classes.item} disableGutters> <Button className={classes.nav} component={CustomRouterLink} to="/users"> Sulphadoxine P.</Button></ListItem>
+            <ListItem className={classes.item} disableGutters> <Button className={classes.nav} component={CustomRouterLink} to="/users"> R.D. Tests</Button></ListItem>
+            <ListItem className={classes.item} disableGutters> <Button className={classes.nav} component={CustomRouterLink} to="/users"> All Commodities</Button></ListItem>
           </List>
         </Collapse>
+
         <ListItem button onClick={handleClick2}>
-          <ListItemText primary="Data Quality" />
-          {open2 ? <ChevronLeftOutlinedIcon /> : <ExpandMore />}
+          <ListItemText primary="Reporting Rate" />
+          {open2 ? <ChevronLeftOutlinedIcon className={classes.chevrons} /> : <ChevronRightOutlined className={classes.chevrons} />}
         </ListItem>
         <Collapse in={open2} timeout="auto" unmountOnExit>
           <List component="div" disablePadding>
-            <ListItem button> &nbsp;<ListItemText primary="Artesunate Inj." /> </ListItem>
-            <ListItem button> &nbsp;<ListItemText primary="Artemether L." /> </ListItem>
-            <ListItem button> &nbsp;<ListItemText primary="Sulphadoxine P." /> </ListItem>
-            <ListItem button> &nbsp;<ListItemText primary="R.D. Tests" /> </ListItem>
-            <ListItem button> &nbsp;<ListItemText primary="All Commodities" /> </ListItem>
+            <ListItem className={classes.item} disableGutters> <Button className={classes.nav} component={CustomRouterLink} to="/users"> Summary</Button></ListItem>
+            <ListItem className={classes.item} disableGutters> <Button className={classes.nav} component={CustomRouterLink} to="/users"> Facility RR</Button></ListItem>
+            <ListItem className={classes.item} disableGutters> <Button className={classes.nav} component={CustomRouterLink} to="/users"> Subcounty RR</Button></ListItem>
           </List>
         </Collapse>
+
         <ListItem button onClick={handleClick3}>
-          <ListItemText primary="Reporting Rate" />
-          {open3 ? <ChevronLeftOutlinedIcon /> : <ExpandMore />}
+          <ListItemText primary="Data Quality" />
+          {open3 ? <ChevronLeftOutlinedIcon className={classes.chevrons} /> : <ChevronRightOutlined className={classes.chevrons} />}
         </ListItem>
         <Collapse in={open3} timeout="auto" unmountOnExit>
           <List component="div" disablePadding>
-            <ListItem button> &nbsp;<ListItemText primary="Artesunate Inj." /> </ListItem>
-            <ListItem button> &nbsp;<ListItemText primary="Artemether L." /> </ListItem>
-            <ListItem button> &nbsp;<ListItemText primary="Sulphadoxine P." /> </ListItem>
-            <ListItem button> &nbsp;<ListItemText primary="R.D. Tests" /> </ListItem>
-            <ListItem button> &nbsp;<ListItemText primary="All Commodities" /> </ListItem>
+            <ListItem className={classes.item} disableGutters> <Button className={classes.nav} component={CustomRouterLink} to="/users"> Consistency</Button></ListItem>
+            <ListItem className={classes.item} disableGutters> <Button className={classes.nav} component={CustomRouterLink} to="/users"> Concordance</Button></ListItem>
+            <ListItem className={classes.item} disableGutters> <Button className={classes.nav} component={CustomRouterLink} to="/users"> Completeness</Button></ListItem>
+            <ListItem className={classes.item} disableGutters> <Button className={classes.nav} component={CustomRouterLink} to="/users"> Comparison</Button></ListItem>
           </List>
         </Collapse>
+
+        <ListItem button onClick={handleClick4}>
+          <ListItemText primary="Supply Chain" />
+          {open4 ? <ChevronLeftOutlinedIcon className={classes.chevrons} /> : <ChevronRightOutlined className={classes.chevrons} />}
+        </ListItem>
+        <Collapse in={open4} timeout="auto" unmountOnExit>
+          <List component="div" disablePadding>
+            <ListItem className={classes.item} disableGutters> <Button className={classes.nav} component={CustomRouterLink} to="/users"> Indicator Summary</Button></ListItem>
+            <ListItem className={classes.item} disableGutters> <Button className={classes.nav} component={CustomRouterLink} to="/users"> Indicator Trends</Button></ListItem>
+          </List>
+        </Collapse>
+        
         {/* ==============multi menu=============== */}
         <ListSubheader style={{color: '#aaaaaa', fontSize: 'small'}}>NATIONAL</ListSubheader>
         <Divider/>
         {/* ==============multi menu=============== */}
-        <ListItem button onClick={handleClick4}>
-          <ListItemText primary="Data Quality" />
-          {open4 ? <ChevronLeftOutlinedIcon /> : <ExpandMore />}
+        <ListItem className={classes.item} disableGutters >
+          <Button activeClassName={classes.active} className={classes.button} component={CustomRouterLink} to="/users" > National summary </Button>
         </ListItem>
-        <Collapse in={open4} timeout="auto" unmountOnExit>
-          <List component="div" disablePadding>
-            <ListItem button>
-              <ListItemText primary="Starred" />
-            </ListItem>
-          </List>
-        </Collapse>
+        <ListItem className={classes.item} disableGutters >
+          <Button activeClassName={classes.active} className={classes.button} component={CustomRouterLink} to="/users" > All Malaria Commodities </Button>
+        </ListItem>
+        <ListItem className={classes.item} disableGutters >
+          <Button activeClassName={classes.active} className={classes.button} component={CustomRouterLink} to="/users" > Pending Shipments </Button>
+        </ListItem>
+        <ListItem className={classes.item} disableGutters >
+          <Button activeClassName={classes.active} className={classes.button} component={CustomRouterLink} to="/users" > Issues vs. Receipts </Button>
+        </ListItem>
         {/* ==============multi menu=============== */}
     </List>
   );
