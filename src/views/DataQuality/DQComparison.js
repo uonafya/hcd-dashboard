85719b4a29400d27107a89b75a4697696ef37a1a
rosaga <richardjohnzranger@gmail.com>
2020-02-17 12:11:49 +0300

    Line charts

--- src/views/DataQuality/DQComparison.js
+++ src/views/DataQuality/DQComparison.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/styles';
 
-import { Toolbar, Table } from './components';
+import { Toolbar, Linechart } from './components';
 import mockData from './data';
 
 const useStyles = makeStyles(theme => ({
@@ -18,11 +18,64 @@ const DQComparison = () => {
 
   const [users] = useState(mockData);
 
+  let dataa = {
+    labels: [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July"
+    ],
+    datasets: [
+      {
+        label: "My First dataset",
+        data: [
+          1592,
+          300,
+          4247,
+          4259,
+          1360,
+          1669,
+          1799
+        ],
+        fill: false,
+        borderDash: [5, 5]
+      }, {
+        hidden: true,
+        label: 'hidden dataset',
+        data: [
+          3921,
+          2158,
+          1529,
+          1437,
+          3584,
+          646,
+          1726
+        ]
+      }, {
+        label: "My Second dataset",
+        data: [
+          911,
+          1208,
+          1389,
+          987,
+          2207,
+          4143,
+          878
+        ]
+      }
+    ]
+  }
+
+
+
   return (
     <div className={classes.root}>
       <Toolbar title="Data Quality: Comparison" />
       <div className={classes.content}>
-        <Table users={users} />
+        <Linechart data = {dataa}  />
       </div>
     </div>
   );
--- src/views/DataQuality/components/index.js
+++ src/views/DataQuality/components/index.js
@@ -1,3 +1,4 @@
 export { default as Table } from './Table';
 export { default as PieChart } from '../../../components/Charts/PieChart';
+export { default as Linechart } from './Line';
 export { default as Toolbar } from './Toolbar';
