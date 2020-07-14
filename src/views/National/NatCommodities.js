8a2e5e90e16378576d5e0080722d0e1a0f170049
Benzer Bett <benzerbett@gmail.com>
2020-07-14 15:59:57 +0300

    all commodities summary in National done

--- src/views/National/NatCommodities.js
+++ src/views/National/NatCommodities.js
@@ -1,30 +1,336 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/styles';
-
+import { Typography } from '@material-ui/core';
+import Alert from '@material-ui/lab/Alert';
+import {
+  filterUrlConstructor,
+  getValidOUs,
+  justFetch
+} from '../../common/utils';
+import { programs } from 'hcd-config';
 import Toolbar from 'components/Toolbar/Toolbar';
-import { DashStockStatus } from 'views/Dashboard/components';
+import NatTable from './components/NatTable/NatTable';
 
+const activProgId = parseFloat(localStorage.getItem('program')) || 1;
+const activProg = programs.filter(pr => pr.id == activProgId)[0];
+const paige = activProg.pages.filter(ep => ep.page == 'All Malaria Commodities')[0];
+const periodFilterType = paige.periodFilter;
+const endpoints = paige.endpoints;
+
+const abortRequests = new AbortController();
+
+const queryString = require('query-string');
 const useStyles = makeStyles(theme => ({
-  root: {
-    padding: theme.spacing(3)
-  },
-  content: {
-    marginTop: theme.spacing(1)
+  root: { padding: theme.spacing(3) },
+  content: { marginTop: theme.spacing(1) },
+  gridchild: {
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(2)
   }
 }));
 
-const NatSummary = () => {
+const NatCommodities = props => {
   const classes = useStyles();
+
+  let filter_params = queryString.parse(props.location.hash);
+  if (
+    filter_params.pe &&
+    filter_params.pe.search(';') > 0 &&
+    periodFilterType != 'range'
+  ) {
+    filter_params.pe = 'LAST_MONTH';
+  }
+  let [url, setUrl] = useState(
+    filterUrlConstructor(
+      filter_params.pe,
+      filter_params.ou,
+      filter_params.level,
+      endpoints[0].local_url
+    )
+  );
+  const [natcomdata, setNatComData] = useState([['Loading...']]);
+  const [prd, setPrd] = useState(null);
+  const [validOUs, setValidOUs] = useState(
+    JSON.parse(localStorage.getItem('validOUs'))
+  );
+  const [oun, setOun] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [oulvl, setOulvl] = useState(null);
+  const [err, setErr] = useState({ error: false, msg: '' });
+  let title = `National: All Commodities`;
+
+  const updateData = (rws, priod, ogu, levl) => {
+    setNatComData(rws);
+    // setPrd(priod)
+    // setOun(ogu)
+    // setOulvl(levl)
+  };
+
+  
+//////// CUSTOM FXNs \\\\\\\\\\\\\\\\\\\\\\\\
+// const getVal = (arry, commo) => {
+// 	let valu = filterItems(arry, commo);
+// 	let thevalue
+//     if (valu[0] != undefined) {
+//         thevalue = valu[0][3];
+//     }
+//     return thevalue;
+// };
+const filterItems = (array, query) => {
+    return array.filter(function (el) {
+        return el.indexOf(query) > -1;
+    });
+};
+const sumArr = (array) => {
+    let sum_total = 0;
+    if (array == null || array == undefined) {
+        array = [0];
+    }
+    array.map((val) => {
+        if (val == null || val == undefined) {
+            val = 0;
+        }
+        sum_total += parseFloat(val);
+    });
+    return sum_total;
+};
+//////// CUSTOM FXNs \\\\\\\\\\\\\\\\\\\\\\\\
+
+
+  let fetchNatComm = async the_url => {
+	setLoading(true);
+	setErr({ error: false, msg: '' });
+    setNatComData([['Loading...']]);
+    try {
+    //   fetch(the_url, { signal: abortRequests.signal })
+      justFetch(the_url, { signal: abortRequests.signal })
+        // .then(s_p => s_p.json())
+        .then(reply => {
+		  if (reply.fetchedData.error) {
+			setLoading(false)
+			setErr({
+              error: true,
+              msg: reply.fetchedData.message,
+              ...reply.fetchedData
+            });
+          } else {
+            setErr({ error: false, msg: '' });
+			/// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+			/// ~~~~~~~~~~~~~~~~~~~~~~ <SUCCESS ~~~~~~~~~~~~~~~~~~~~~~~~~~
+			/// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+			let tableData = [];
+			let dxidsadjc = [];
+			let dxidskemsa = [];
+			let dxidshfs = [];
+
+			let count = 0;
+
+			let products = [ "Artemether-Lumefantrine 20/120 Tabs 6s", "Artemether-Lumefantrine 20/120 Tabs 12s", "Artemether-Lumefantrine 20/120 Tabs 18s", "Artemether-Lumefantrine 20/120 Tabs 24s", "Artesunate Injection", "Sulphadoxine Pyrimethamine Tabs", "Rapid Diagnostic Tests" ];
+
+			let dxuom = [ "doses", "doses", "doses", "doses", "vials", "tablets", "tests", ];
+
+			reply.fetchedData.metaData.dimensions.dx.map( (dxentry) => {
+				if (count <= 6) {
+					dxidsadjc.push(dxentry);
+				}
+				if (count > 6 && count <= 13) {
+					dxidskemsa.push(dxentry);
+				}
+				if (count > 13 && count <= 20) {
+					dxidshfs.push(dxentry);
+				}
+
+				count++;
+			});
+
+			let adjcvalues = [];
+			let kemsavalues = [];
+			let hfsvalues = [];
+
+			dxidsadjc.map( (rowentry) => {
+				reply.fetchedData.rows.map(rowentry2 => {
+					if (rowentry == rowentry2[0]) {
+						adjcvalues.push(parseFloat(rowentry2[3]));
+					}
+				});
+			});
+
+			let kemsaidvals = [];
+			dxidskemsa.map( (rowentry) => {
+				reply.fetchedData.rows.map( (rowentry2) => {
+					if (rowentry2[0] == rowentry) {
+						if (kemsaidvals.indexOf(rowentry2[0]) >= 0) {
+						} else {
+							kemsaidvals.push(rowentry2[0]);
+						}
+					}
+				});
+			});
+
+			//get kemsa values
+			dxidskemsa.map( (rowentry) => {
+				if (kemsaidvals.indexOf(rowentry) >= 0) {
+					reply.fetchedData.rows.map( (rowentry2) => {
+						if (rowentry == rowentry2[0]) {
+							if (rowentry2[3]) {
+								kemsavalues.push(rowentry2[3]);
+							} else {
+								kemsavalues.push(0);
+							}
+						}
+					});
+				} else {
+					kemsavalues.push(0);
+				}
+			});
+
+			//get hfs values
+			dxidshfs.map( ( rowentry) => {
+				reply.fetchedData.rows.map( ( rowentry2) => {
+					if (rowentry == rowentry2[0]) {
+						hfsvalues.push(rowentry2[3]);
+					}
+				});
+			});
+
+			for (let i = 0; i < products.length; i++) {
+				let trow = []
+
+				if (typeof kemsavalues[i] == "undefined") kemsavalues[i] = 0;
+
+				if (typeof hfsvalues[i] == "undefined") hfsvalues[i] = 0;
+
+				if (typeof adjcvalues[i] == "undefined") adjcvalues[i] = 0;
+
+				let total =
+					parseFloat(kemsavalues[i]) + parseFloat(hfsvalues[i]);
+
+				let kemsamos = kemsavalues[i] / adjcvalues[i];
+
+				let hfsmos = hfsvalues[i] / adjcvalues[i];
+
+				let totalmos = total / adjcvalues[i];
+
+				trow.push(products[i]);
+				trow.push(dxuom[i]);
+				trow.push( Math.trunc(adjcvalues[i]).toLocaleString() );
+				trow.push( Math.trunc(kemsavalues[i]).toLocaleString() );
+				trow.push( Math.trunc(hfsvalues[i]).toLocaleString() );
+				trow.push( Math.trunc(total).toLocaleString() );
+				trow.push( kemsamos.toFixed(1) );
+				trow.push( hfsmos.toFixed(1) );
+				trow.push( totalmos.toFixed(1) );
+
+				tableData.push(trow)
+			}
+			let o_gu = reply.fetchedData.metaData.items[reply.fetchedData.metaData.dimensions.ou[0]].name || oun
+			updateData( tableData, reply.fetchedData.metaData.items[ reply.fetchedData.metaData.dimensions.pe[0] ].name, o_gu, oulvl );
+			/// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+			/// ~~~~~~~~~~~~~~~~~~~~~~ SUCCESS/> ~~~~~~~~~~~~~~~~~~~~~~~~~~
+			/// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+          }
+          setLoading(false);
+        })
+        .catch(err => {
+          setLoading(false);
+          setErr({ error: true, msg: 'Error fetching data', ...err });
+        });
+    } catch (er) {
+      setErr({ error: true, msg: 'Error fetching data', ...er });
+    }
+  };
+
+  const onUrlChange = base_url => {
+    props.history.listen((location, action) => {
+      let new_filter_params = queryString.parse(location.hash);
+      if (
+        new_filter_params.pe != '~' &&
+        new_filter_params.pe != '' &&
+        new_filter_params.pe != null
+      ) {
+        setPrd(new_filter_params.pe);
+      }
+      if (
+        new_filter_params.ou != '~' &&
+        new_filter_params.ou != '' &&
+        new_filter_params.ou != null
+      ) {
+        setOun(new_filter_params.ou);
+      }
+      if (
+        new_filter_params.level != '~' &&
+        new_filter_params.level != '' &&
+        new_filter_params.level != null
+      ) {
+        setOulvl(new_filter_params.level);
+      }
+      let new_url = filterUrlConstructor(
+        new_filter_params.pe,
+        new_filter_params.ou,
+        new_filter_params.level,
+        base_url
+      );
+      fetchNatComm(new_url);
+    });
+  };
+
+  useEffect(() => {
+    fetchNatComm(url);
+    onUrlChange(endpoints[0].local_url);
+    getValidOUs().then(vo => {
+      let vFlS = JSON.parse(localStorage.getItem('validOUs'));
+      if (vFlS && vFlS.length < 1) {
+        setValidOUs(vo);
+        // localStorage.removeItem('validOUs')
+        // console.log("refetching validOUs with getValidOUs")
+        // localStorage.setItem('validOUs', JSON.stringify(vo))
+      }
+    });
+
+    return () => {
+      console.log(`Acc: aborting requests...`);
+      abortRequests.abort();
+    };
+  }, []);
+
+  let data = {};
+  data.theads = [
+    'Commodity',
+    'Unit',
+    'Adjusted AMC',
+    'KEMSA SOH',
+    'Facilities SOH',
+    'Total SOH',
+    'KEMSA MOS',
+    'Facilities MOS',
+    'Total MOS'
+  ];
+  data.rows = natcomdata;
 
   return (
     <div className={classes.root}>
-      <Toolbar title={"National: All Commodities"} pe={"prd"} ou={"oun"} lvl={null} />
+      <Toolbar
+        className={classes.gridchild}
+        title={title}
+        pe={prd}
+        ou={oun}
+        lvl={oulvl}
+        filter_params={filter_params}
+      />
       <div className={classes.content}>
-        {/* <DashStockStatus /> */}
-        <DashStockStatus />
+        {err.error ? (
+          <Alert severity="error">{err.msg}</Alert>
+        ) : (
+          <NatTable
+            pageTitle={title}
+            theads={data.theads}
+            rows={data.rows}
+            loading={loading.toString()}
+		  />
+        )}
       </div>
     </div>
   );
 };
 
-export default NatSummary;
+export default NatCommodities;
