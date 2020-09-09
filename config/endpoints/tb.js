1ef39dc495b99216ca35aba16cf60d5c588aa334
Benzer Bett <benzerbett@gmail.com>
2020-09-09 10:44:08 +0300

    modularizing config

---
+++ config/endpoints/tb.js
@@ -0,0 +1,6 @@
+let DHIS_BASE_API_URL = process.env.REACT_APP_DHIS_BASE_API_URL;
+let APP_BASE_URL =
+  process.env.REACT_APP_APP_BASE_URL || 'http://41.89.94.99:3000';
+let tb = []
+
+  module.exports = tb
