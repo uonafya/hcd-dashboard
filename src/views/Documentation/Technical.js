315a0e2168dccfe03876bcc64e8db721f34635d9
Benzer Bett <benzerbett@gmail.com>
2020-11-30 14:24:20 +0300

    minor additions

--- src/views/Documentation/Technical.js
+++ src/views/Documentation/Technical.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const Technical = () => {
-	let u_rl = window.location.href.replace('/index.html', '').replace(window.location.hash, '') + '/docs/index.html';
-	// u_rl = '/docs/index.html';
+	let u_rl = window.location.href.replace('/index.html', '').replace(window.location.hash, '')
+	if(u_rl[u_rl.length - 1] != "/"){u_rl += '/docs/index.html'}else{u_rl += 'docs/index.html'}
 	return (
 		<div className="full-width">
-			<iframe src={u_rl} title="Technical Documentation" className="full-width" frameBorder="0"style={{width: '100%', height: '100%', minHeight: '97vh'}}>
+			<iframe src={u_rl} title="Technical Documentation" className="full-width" frameBorder="0" style={{width: '100%', height: '100%', minHeight: '97vh'}}>
 			</iframe>
 		</div>
 	)
